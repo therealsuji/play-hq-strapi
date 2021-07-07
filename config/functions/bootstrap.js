545d695c4e0a7343387eea110e05b71a0bc2c381
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-07-07 19:00:38 +0530

    changes

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,13 +1,11 @@
-'use strict';
+'use strict'
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
- */
+const admin = require("firebase-admin")
+const serviceAccount = require("../../serviceAccountKey.json")
 
-module.exports = () => {};
+module.exports = () => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+  strapi.firebase = admin
+}
