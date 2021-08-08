a30ad89233cfc811fd8eaaea7362b492994e9eb9
Sujitha Wijewantha <39885793+therealsuji@users.noreply.github.com>
2021-08-08 13:00:53 +0530

    made fields non nullable

--- api/sell-games/controllers/sell-games.js
+++ api/sell-games/controllers/sell-games.js
@@ -10,6 +10,7 @@ module.exports = {
       entity = await strapi.services["sell-games"].create(data, { files });
     } else {
       ctx.request.body.user_id = ctx.state.user.id;
+      delete ctx.request.body.status;
       entity = await strapi.services["sell-games"].create(ctx.request.body);
     }
     return sanitizeEntity(entity, { model: strapi.models["sell-games"] });
